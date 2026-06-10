6c2bb9d26dc4ce2e3b9a3d9704a335afbdbb9370
DidierLeBail <didierm.lebail@gmail.com>
2026-06-10 12:17:10 +0200

    improved the user model by taking into account impossible actions

--- src_code/bayesian_info_gain/main.py
+++ src_code/bayesian_info_gain/main.py
@@ -26,7 +26,7 @@ class User:
     
     Attributes
     ----------
-    theta : int
+    goal : int
         the user target
     _prior : Prior
         the user true prior (the target is drawn from it)
@@ -39,20 +39,20 @@ class User:
         prior: Prior,
         policy: Callable
     ):
-        self.theta = None
+        self.goal = None
         self._prior = prior
         self._policy = policy
 
     def pick_goal(self, rng: np.random.Generator):
         """The user picks a goal."""
-        self.theta = self._prior.draw(rng)
+        self.goal = self._prior.draw(rng)
 
     def pick_action(self, stimulus: int):
         """Apply the user policy:
-        it determines the use action from its observation of the world (`stimulus`) and its own hidden state (`self._theta`).
+        it determines the use action from its observation of the world (`stimulus`) and its own hidden state (`self.goal`).
         Note that contrary to a RL agent, this hidden state does not change, because it reflects the user goal.
         """
-        return self._policy(stimulus, self.theta)
+        return self._policy(stimulus, self.goal)
 
 class Computer:
     """In the BIG framework, the computer tries to guess the user's goal by sending to the user
@@ -69,13 +69,12 @@ class Computer:
 
     Parameters
     ----------
-    prior : numpy.ndarray
+    prior : Prior
         the prior about the user goal, that here lives in the same space as the stimulus
         (it is an integer between 0 and `nb_stim - 1`) ;
-        this prior is updated according to the user model and user action ;
-        here the prior consists in a list with `self.prior[i] = proba of self.thetas[i]`
-    model : Distribution
-        the modeling of the user, as a distribution over user actions conditioned on user goal and stimulus ;
+        this prior is updated according to the user model and user action
+    model : Callable
+        the model describing the user, as a distribution over user actions conditioned on user goal and stimulus ;
         is not updated, and may differ from the actual user policy
     nb_stim : int
         the nb of possible stimulus (size of the stimulus space)
@@ -84,13 +83,13 @@ class Computer:
     
     Attributes
     ----------
-    thetas : numpy.ndarray
+    goals : numpy.ndarray
         list of all possible values for the user goal
         (here same values as the stimulus)
     """
 
     def __init__(self,
-        prior: np.ndarray,
+        prior: Prior,
         model: Callable,
         nb_stim: int,
         nb_actions: int
@@ -98,14 +97,18 @@ class Computer:
         self.prior = prior
         self.model = model
         self.nb_stim = nb_stim
-        self.thetas = np.arange(nb_stim, dtype=int)
+        self.goals = np.arange(nb_stim, dtype=int)
         self.nb_actions = nb_actions
     
+    def init_stimulus(self, rng: np.random.Generator):
+        """Here drawing a stimulus is the same as drawing a goal."""
+        return self.prior.draw(rng)
+
     def update_prior(self, stimulus: int, user_action: int):
         """Updates `self.prior` the distribution over the user's goals.
         """
-        self.prior = self.prior * self.model(user_action, stimulus, self.thetas)
-        self.prior /= np.sum(self.prior)
+        self.prior.probas *= self.model(user_action, stimulus, self.goals)
+        self.prior.probas /= np.sum(self.prior.probas)
 
     def utility(self, stimulus: int):
         """Computes the utility of `stimulus`:
@@ -115,14 +118,14 @@ class Computer:
         # compute the entropy of the distribution of user actions conditioned on the user stimulus
         p_yx = np.zeros(self.nb_actions)
         for user_action in range(self.nb_actions):
-            p_yx[user_action] = np.sum( self.prior * self.model(user_action, stimulus, self.thetas) )
+            p_yx[user_action] = np.sum( self.prior.probas * self.model(user_action, stimulus, self.goals) )
         H_yx = - np.sum( p_yx * np.log2(p_yx) )
 
         # compute the entropy of the distribution of user actions conditioned on user goal and stimulus
         q_yx = np.zeros(self.nb_actions)
         for user_action in range(self.nb_actions):
-            mod = self.model(user_action, stimulus, self.thetas)
-            q_yx[user_action] = np.sum( self.prior * mod * np.log2(mod) )
+            mod = self.model(user_action, stimulus, self.goals)
+            q_yx[user_action] = np.sum( self.prior.probas * mod * np.log2(mod) )
         H_yThetaX = - np.sum(q_yx)
 
         # deduce the mutual information between user goal and action, conditioned on stimulus
@@ -142,28 +145,34 @@ class Computer:
 
 if __name__ == "__main__":
     # define the user
-    nb_thetas = 100
-    probas = np.ones(nb_thetas) / nb_thetas
-    true_prior = Prior(nb_thetas, probas)
+    nb_goals = 20
+    probas = np.ones(nb_goals) / nb_goals
+    true_prior = Prior(nb_goals, probas)
 
-    nb_stim = nb_thetas
+    nb_stim = nb_goals
     nb_actions = 3
-    def true_policy(stimulus, theta):
+    def true_policy(stimulus: int, goal: int):
         """Returns the user action given stimulus and goal."""
-        return ( 2 * np.heaviside(stimulus - theta, 0.5) ).astype(int)
+        return np.heaviside(stimulus - goal, 2).astype(int)
 
     user = User(true_prior, true_policy)
 
     # define the computer
-    prior = probas
+    prior = Prior(nb_goals, probas)
 
-    def model(action: int, stimulus: int, thetas: Union[int, np.ndarray]):
+    def model(action: int, stimulus: int, goals: Union[int, np.ndarray]):
         """Returns the proba of a user action given a stimulus and user goal.
         
         Note that the probas associated to different goals can be computed in a single pass,
-        if `thetas` is a `numpy.ndarray`.
+        if `goals` is a `numpy.ndarray`.
+
+        Notes
+        -----
+        At the boundary, the model should take into account that one of the user actions is impossible.
+        Otherwise, it takes much longer for the computer to guess the user goal when this goal matches with one of the boundaries
+        (it takes a linear time instead of a logarithmic time).
         """
-        true_a = true_policy(stimulus, thetas)
+        true_a = true_policy(stimulus, goals)
         a1 = (true_a + 1) % 3
         a2 = (true_a + 2) % 3
 
@@ -172,8 +181,25 @@ if __name__ == "__main__":
         lamb2 = 1 / 8
 
         tab = action + np.zeros_like(true_a)
+        old_proba = lamb * (tab == true_a) + lamb1 * (tab == a1) + lamb2 * (tab == a2)
 
-        return lamb * (tab == true_a) + lamb1 * (tab == a1) + lamb2 * (tab == a2)
+        # the action 1 is impossible and so the probas of 0 and 2 must be renormalized
+        if stimulus == 0:
+            if action == 1:
+                return np.zeros_like(goals) + 1e-14
+            
+            norm = np.where(goals > 0, 1 - lamb1, 1 - lamb2)
+            return old_proba / norm
+        
+        # the action 0 is impossible
+        elif stimulus == nb_stim - 1:
+            if action == 0:
+                return np.zeros_like(goals) + 1e-14
+            
+            norm = np.where(goals < nb_stim - 1, 1 - lamb2, 1 - lamb1)
+            return old_proba / norm
+            
+        return old_proba
 
     computer = Computer(prior, model, nb_stim, nb_actions)
 
@@ -181,16 +207,16 @@ if __name__ == "__main__":
     rng = np.random.default_rng(seed=None)
     user.pick_goal(rng)
     
-    print(f"goal chosen by the user: {user.theta}")
+    print(f"goal chosen by the user: {user.goal}")
     print()
 
-    stim = rng.choice(computer.nb_stim, p=computer.prior)
+    stim = computer.init_stimulus(rng)
 
     print(f"first stimulus sent by the computer: {stim}")
     print()
 
     nb_trials = 0
-    while stim != user.theta:
+    while stim != user.goal:
         action = user.pick_action(stim)
         stim = computer.pick_stimulus(stim, action)
         #print(f"new stimulus sent by the computer: {stim}")
